32f750644329128321f40bb1ddc7052e39a9e5ce
Santiago Ocana barrera <socana-b@c3r9s3.42madrid.com>
2022-10-26 11:20:35 +0200

    cositas

--- Philosopher/thread_tutorial/volatile.c
+++ Philosopher/thread_tutorial/volatile.c
@@ -3,23 +3,30 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void	*rutine()
-{
-	sleep(5);
-	printf("Test from threads\n");
-	return (NULL);
-}
+
+/*
+	// pthread_create //
+	Creates a thread as of a function
+	Param:
+		1) Address from memory
+		2) Atributes for the thread, if you put NULL the thread will run with defaults options
+		3) Address of the function
+		4) Arguments for the functions(void * format), if there no arguments put NULL
+	Return: 0 works correctly, other there was an error
+
+	// pthread_join //
+	This works like waitpid but with threads
+	Params: 1) Variable with thread 2) The return of the thread
+	Return: 0 works correctly, other there was an error
+
+	// pthread_mutex //
+	
+*/
 
 int main (void)
 {
-	pthread_t t1;
-	//Creates a thread as of a function
-	// Param: 1) Address from memory 2) Atributes for the thread, if you put NULL the thread will run with defaults options 3) Address of the function 4) Arguments for the functions, if there no arguments put NULL
-	// Return: 0 works correctly, other there was an error
-	pthread_create(&t1, NULL, &rutine, NULL);
-	// This works like waitpid but with threads
-	// Params: 1) Variable with thread 2) The return of the thread
-	// Return: 0 works correctly, other there was an error
-	pthread_join(t1, NULL);
+	pthread_t t1, t2;
+	int x = 5;
+
 	return (0);
 }

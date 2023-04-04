e79b21a5ceeb07dd1dbcf340a9ce0357dd6fe597
BlueFlare <blue.flare.011@gmail.com>
2023-04-04 07:01:46 +0200

    miau

--- philosophers/utils/philo_utils.c
+++ philosophers/utils/philo_utils.c
@@ -2,7 +2,6 @@
 
 unsigned int getTime(struct timeval *end, struct timeval *init)
 {
-	gettimeofday(end, NULL);
 	return(end->tv_sec * 1000 + end->tv_usec / 1000)
 		- (init->tv_sec * 1000 + init->tv_usec / 1000);
 }
@@ -11,6 +10,9 @@ void smartPrint(char *message, t_thread_data *data)
 {
 	pthread_mutex_lock(data->param->w_alive);
 	if (data->param->alive)
+	{
+		gettimeofday(&data->end, NULL);
 		printf("%ums %d %s\n", getTime(&data->end, &data->init), data->id, message);
+	}
 	pthread_mutex_unlock(data->param->w_alive);
 }

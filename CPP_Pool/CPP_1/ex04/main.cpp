786dbc356555a6cb4aac3a9a8710d05acbe3b4f7
Santiago Ocana barrera <socana-b@c2r17s1.42madrid.com>
2024-01-30 21:59:10 +0100

    Me quiero pegar un tiro

--- CPP_Pool/CPP_1/ex04/main.cpp
+++ CPP_Pool/CPP_1/ex04/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <fstream> 
 #include <string.h>
+
+void replace(std::string outLine, char *substr, int start, int end)
+{
+	int j = 0;
+	while (j < strlen(substr) && j < end){
+		outLine[start + j] = substr[j];
+	}
+	if (j < end){
+		outLine.resize(outLine.length() + (end - j));
+		while (j < end){
+			char aux = outLine[start + j];
+			outLine[start + j] = substr[j];
+		}
+	}else{
+		outLine.erase(start + j, start + end);
+	}
+}
+
 int main(int argc, char **argv)
 {
 	if (argc != 4){
@@ -14,9 +32,15 @@ int main(int argc, char **argv)
 	std::ofstream Writer(outfile.append(".replace"));
 
 	while (getline(Reader, auxLine)) {
-		for (int i = 0; i < auxLine.length(); i++)
+		outLine = auxLine;
+		for (int i = 0; i < auxLine.length(); i++){
 			if (!auxLine.compare(i, strlen(argv[2]), argv[2])){
-				
+				replace(outLine, argv[3], i, strlen(argv[2]));
+				std::cout << outLine << std::endl;
 			}
+		}
+		Writer << outLine;
 	}
+	Writer.close();
+	Reader.close();
 }
---
+++ CPP_Pool/CPP_1/ex04/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fstream> 
+
+int main(int argc, char **argv)
+{
+	std::string str = "Hola";
+	return 1;
+}

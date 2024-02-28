96bd8f7670d167f3bf1b6d297df6e2a77034d502
Santiago Ocana barrera <socana-b@c1r12s4.42madrid.com>
2024-02-28 20:23:47 +0100

    Vamos a casa a piratearnos la ds

--- CPP_Pool/CPP_2/ex00/Fixed.cpp
+++ CPP_Pool/CPP_2/ex00/Fixed.cpp
@@ -4,16 +4,25 @@
 Fixed::Fixed()
 {
 	std::cout << "Default constructor called" << std::endl;
+	this->point = 0;
 }
 
 Fixed::Fixed(const Fixed& fixed)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
+	std::cout << "Copy constructor called" << std::endl;
+	this->point = fixed.getRawBits();
 }
 
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed&	Fixed::operator=(const Fixed& fixed)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->point = fixed.getRawBits();
+	return (*this);
 }
 
 int Fixed::getRawBits(void) const
